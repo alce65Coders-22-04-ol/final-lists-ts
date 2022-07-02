26ccf6c6c7e8e4a31e9c82ec4d8432904fbc8611
Alejandro Cerezo <alce65@hotmail.es>
2022-07-02 21:36:53 +0200

    Add initial Tasks Page

---
+++ src/features/tasks/pages/tasks.tsx
@@ -0,0 +1,12 @@
+import tasks from './tasks.module.css';
+
+function TasksPage() {
+    return (
+        <section className={tasks}>
+            <h2>Página Tasks</h2>
+            <p>Lista de tareas</p>
+        </section>
+    );
+}
+
+export default TasksPage;
