88ed7fee5c585a7d5eea6aac8b2f6a7ce18cf89e
Alejandro Cerezo <alce65@hotmail.es>
2022-07-10 09:15:46 +0200

    Update Home Page HTML

--- src/features/home/pages/home.tsx
+++ src/features/home/pages/home.tsx
@@ -4,6 +4,7 @@ import { CounterClicks } from '../components/counter-clicks/counter-clicks';
 import { CounterStates } from '../components/counter-states/counter-states';
 
 import home from './home.module.css';
+import { TaskContextProvider } from '../../tasks/context/provider';
 
 function HomePage() {
     const [totalClicks, setTotalClicks] = useState(0);
@@ -13,7 +14,7 @@ function HomePage() {
     };
 
     return (
-        <>
+        <section>
             <h2>Página Home</h2>
             <p>Total clicks: {totalClicks}</p>
             <div className={home.counters}>
@@ -21,7 +22,7 @@ function HomePage() {
                 <CounterClicks setTT={addClick}></CounterClicks>
                 <CounterStates setTT={addClick}></CounterStates>
             </div>
-        </>
+        </section>
     );
 }
 
